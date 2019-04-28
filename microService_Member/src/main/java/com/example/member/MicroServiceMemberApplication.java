b7c1e8b33d8bf8507e5cc0e3e464a6e371f6f5a2
SonMangeun <sonmg0416@gmail.com>
2019-04-29 01:08:43 +0900

    마이크로서비스_member

---
+++ microService_Member/src/main/java/com/example/member/MicroServiceMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceMemberApplication.class, args);
+	}
+
+}
